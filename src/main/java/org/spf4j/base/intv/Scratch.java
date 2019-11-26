2d20d932220e90cf43df392c5c1132403dd7e908
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-11-26 06:40:18 -0500

    [cleanup]

--- src/main/java/org/spf4j/base/intv/Scratch.java
+++ src/main/java/org/spf4j/base/intv/Scratch.java
@@ -2,15 +2,9 @@ package org.spf4j.base.intv;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class Scratch {
 
